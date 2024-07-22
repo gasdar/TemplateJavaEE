fd846f4e41e269960fb047b03bcf9e9e7f175fae
gasdar <gottafindshape@gmail.com>
2024-07-21 22:16:58 -0400

    Template Java EE

---
+++ src/main/java/com/rolan/javaee/servlets/SvUser.java
@@ -0,0 +1,40 @@
+package com.rolan.javaee.servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "SvUser", urlPatterns = {"/SvUser"})
+public class SvUser extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+    
+}
---
+++ target/JavaEE-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,18 @@
+<%--
+    Nada más que decir, hay que iniciar
+--%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Java EE</title>
+    </head>
+    <body>
+        
+        <h1>Hola mundo desde HTML!</h1>
+        <% String saludo = "Hola mundo desde Java!"; %>
+        <div><%=saludo%></div>
+        
+    </body>
+</html>
